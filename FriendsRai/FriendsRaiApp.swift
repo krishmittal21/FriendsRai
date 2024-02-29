ffafb0ef520de64add9c943c68df316df9506f96
Krish Mittal <krishmittal212005@gmail.com>
2024-02-29 09:26:50 +0530

    Initial Commit

---
+++ FriendsRai/FriendsRaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsRaiApp.swift
+//  FriendsRai
+//
+//  Created by Krish Mittal on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendsRaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
