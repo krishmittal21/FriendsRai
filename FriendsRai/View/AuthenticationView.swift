c813d81a82f5d899e755baa39bcbc987c89f8169
Krish Mittal <krishmittal212005@gmail.com>
2024-02-29 18:12:04 +0530

    spline-3d-design

--- FriendsRai/View/AuthenticationView.swift
+++ FriendsRai/View/AuthenticationView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SplineRuntime
 
 struct AuthenticationView: View {
     
@@ -16,59 +17,97 @@ struct AuthenticationView: View {
         NavigationView{
             
             VStack{
-                Button{
-                    Task {
-                        await viewModel.signInWithGoogle()
-                    }
-                } label: {
-                    HStack{
-                        Image("google")
-                            .resizable()
-                            .frame(width: 20,height: 20)
-                        Text("SignIn with Google")
-                            .foregroundStyle(Color.blackColor)
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 8)
-                }
-                .buttonStyle(.bordered)
-                
-                NavigationLink(destination: EmailSignUpView()) {
-                    HStack{
-                        Image(systemName: "envelope.fill")
-                            .resizable()
-                            .frame(width: 25,height: 20)
-                            .foregroundStyle(Color.primaryColor)
-                        Text("SignUp with Email")
-                        
-                            .foregroundStyle(Color.blackColor)
-                        
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 8)
-                }
-                .buttonStyle(.bordered)
-                .navigationTitle("")
-                .navigationBarBackButtonHidden(true)
-                
-                HStack{
-                    Text("Already have an account?")
-                        .foregroundStyle(Color.gray)
-                    NavigationLink{
-                        EmailLoginView()
+                Onboard3DView()
+                    .frame(height: 500)
+                    .ignoresSafeArea()
+
+                VStack{
+                    Button{
+                        Task {
+                            await viewModel.signInWithGoogle()
+                        }
                     } label: {
-                        Text("Log In")
-                            .underline()
-                            .foregroundStyle(Color.primaryColor)
+                        HStack{
+                            Image("google")
+                                .resizable()
+                                .frame(width: 20,height: 20)
+                            Text("SignIn with Google")
+                                .foregroundStyle(Color.blackColor)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 8)
                     }
+                    .buttonStyle(.bordered)
+                    
+                    Button{
+                        Task {
+                            
+                        }
+                    } label: {
+                        HStack{
+                            Image("apple")
+                                .resizable()
+                                .frame(width: 20,height: 20)
+                            Text("SignIn with Apple")
+                                .foregroundStyle(Color.blackColor)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 8)
+                    }
+                    .buttonStyle(.bordered)
+                    
+                    NavigationLink(destination: EmailSignUpView()) {
+                        HStack{
+                            Image(systemName: "envelope.fill")
+                                .resizable()
+                                .frame(width: 25,height: 20)
+                                .foregroundStyle(Color.primaryColor)
+                            Text("SignUp with Email")
+                            
+                                .foregroundStyle(Color.blackColor)
+                            
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 8)
+                    }
+                    .buttonStyle(.bordered)
+                    .navigationTitle("")
+                    .navigationBarBackButtonHidden(true)
+                    
+                    HStack{
+                        Text("Already have an account?")
+                            .foregroundStyle(Color.gray)
+                        NavigationLink{
+                            EmailLoginView()
+                        } label: {
+                            Text("Log In")
+                                .underline()
+                                .foregroundStyle(Color.primaryColor)
+                        }
+                    }
+                    .padding(10)
                 }
-                .padding(10)
+                .padding(40)
             }
-            .padding(40)
         }
     }
+    
 }
 
 #Preview {
     AuthenticationView()
+}
+
+
+struct Onboard3DView: View {
+    var body: some View {
+        
+        // fetching from cloud
+        let url = URL(string: "https://build.spline.design/zL2dZLu7DVCtZbhXqN35/scene.splineswift")!
+        
+        // // fetching from local
+        // let url = Bundle.main.url(forResource: "scene", withExtension: "splineswift")!
+        
+        try? SplineView(sceneFileURL: url)
+    }
 }
