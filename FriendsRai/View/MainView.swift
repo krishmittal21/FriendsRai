964c7c1fb0399aa8abe66c67a30f6267dcbb25ca
Krish Mittal <krishmittal212005@gmail.com>
2024-02-29 15:08:40 +0530

    logout

--- FriendsRai/View/MainView.swift
+++ FriendsRai/View/MainView.swift
@@ -11,7 +11,7 @@ struct MainView: View {
     @StateObject var viewModel = MainViewModel()
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-            HomeView()
+            ProfileView()
         } else {
             AuthenticationView()
         }
