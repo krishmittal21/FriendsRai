310e2219238220a2a45bb5885128f2905ae0b7bb
Krish Mittal <krishmittal212005@gmail.com>
2024-03-01 11:46:02 +0530

    naigation

---
+++ FriendsRai/View/HelplineNumberView.swift
@@ -0,0 +1,18 @@
+//
+//  HelplineNumberView.swift
+//  FriendsRai
+//
+//  Created by Krish Mittal on 01/03/24.
+//
+
+import SwiftUI
+
+struct HelplineNumberView: View {
+    var body: some View {
+        Text("List")
+    }
+}
+
+#Preview {
+    HelplineNumberView()
+}
---
+++ FriendsRai/View/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapsView.swift
+//  FriendsRai
+//
+//  Created by Krish Mittal on 01/03/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+#Preview {
+    MapView()
+}
