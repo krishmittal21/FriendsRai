bcccca433c03df700e14459b80c160e2a45740b9
Krish Mittal <krishmittal212005@gmail.com>
2024-03-08 09:55:25 +0530

    Add-Friend

--- FriendsRai/View/AddFriendsView.swift
+++ FriendsRai/View/AddFriendsView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct AddFriendsView: View {
+    @StateObject var viewModel = AddFriendsViewModel()
     var body: some View {
         NavigationView{
             VStack {
                 HStack{
                     Text("Edit Friends")
                     Spacer()
-                    Image(systemName: "person.crop.circle.fill.badge.plus")
-                    Image(systemName: "minus.circle.fill")
+                    Image(systemName: "person.crop.circle.fill.badge.plus").imageScale(.medium)
+                        .onTapGesture {
+                            viewModel.openContactPicker()
+                        }
+                    Image(systemName: "minus.circle.fill").imageScale(.medium)
                 }
                 .padding()
                 Spacer()
--- FriendsRai/ViewModel/AddFriendsViewModel.swift
+++ FriendsRai/ViewModel/AddFriendsViewModel.swift
@@ -8,18 +8,55 @@
 import Foundation
 import FirebaseAuth
 import FirebaseFirestore
+import ContactsUI
+import Combine
 
-class AddFriendsViewModel: ObservableObject {
+class AddFriendsViewModel:  NSObject, ObservableObject, CNContactPickerDelegate {
     
     @Published var user:User? = nil
     @Published var contact:EmergencyContact? = nil
     @Published var contactName = ""
     @Published var contactNumber = ""
+
     
-    init(){}
+    func openContactPicker() {
+        let contactPicker = CNContactPickerViewController()
+        contactPicker.delegate = self
+        contactPicker.displayedPropertyKeys = [CNContactPhoneNumbersKey, CNContactGivenNameKey]
+        contactPicker.predicateForEnablingContact = NSPredicate(format: "phoneNumbers.@count > 0")
+        contactPicker.predicateForSelectionOfContact = NSPredicate(format: "phoneNumbers.@count == 1")
+        contactPicker.predicateForSelectionOfProperty = NSPredicate(format: "key == 'phoneNumbers'")
+        
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScenes = scenes.first as? UIWindowScene
+        let window = windowScenes?.windows.first
+        window?.rootViewController?.present(contactPicker, animated: true, completion: nil)
+    }
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        contactName = contact.givenName
+        if let phoneNumber = contact.phoneNumbers.first?.value {
+            handlePhoneNumber(phoneNumber.stringValue)
+        }
+        saveContact()
+    }
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contactProperty: CNContactProperty) {
+        if contactProperty.key == CNContactPhoneNumbersKey,
+           let phoneNumber = contactProperty.value as? CNPhoneNumber {
+            contactName = contactProperty.contact.givenName
+            handlePhoneNumber(phoneNumber.stringValue)
+        }
+        saveContact()
+    }
+    
+    func handlePhoneNumber(_ phoneNumber: String) {
+        let phoneNumberWithoutSpace = phoneNumber.replacingOccurrences(of: " ", with: "")
+        contactNumber = phoneNumberWithoutSpace
+    }
     
     func saveContact(){
-        
+                
         guard let uId = Auth.auth().currentUser?.uid else {
             return
         }
