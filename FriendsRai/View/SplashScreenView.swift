14e0c54e35846d84c8412a684a39a79787621b7e
Krish Mittal <krishmittal212005@gmail.com>
2024-03-12 15:57:39 +0530

    splash-screen

---
+++ FriendsRai/View/SplashScreenView.swift
@@ -0,0 +1,38 @@
+//
+//  SplashScreenView.swift
+//  FriendsRai
+//
+//  Created by Krish Mittal on 12/03/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    
+    @State var isActive: Bool = false
+    
+    var body: some View {
+        ZStack{
+            if self.isActive {
+                MainView()
+            } else {
+                Color("SplashColor").edgesIgnoringSafeArea(.all)
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
