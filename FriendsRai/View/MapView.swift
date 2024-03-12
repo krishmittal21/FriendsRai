38c82f428dab6fd270416cf6d7996dd950ff543e
Krish Mittal <krishmittal212005@gmail.com>
2024-03-12 12:50:23 +0530

    selectedLocationType-button

--- FriendsRai/View/MapView.swift
+++ FriendsRai/View/MapView.swift
@@ -11,16 +11,24 @@ import MapKit
 struct MapView: View {
     
     @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    @State private var selectedLocationType: LocationType? = nil
     
     var body: some View {
         Map(position: $position) {
             
             UserAnnotation()
             
-            ForEach(LocationType.allCases, id: \.self) { locationType in
-                ForEach(locationType.verifiedLocations, id: \.self) { location in
-                    Marker(location.name, systemImage: locationType.image, coordinate: CLLocationCoordinate2D(latitude: CLLocationDegrees(location.latitude), longitude: CLLocationDegrees(location.longitude)))
-                        .tint(locationType.color)
+            if let selectedType = selectedLocationType {
+                ForEach(selectedType.verifiedLocations, id: \.self) { location in
+                    Marker(location.name, systemImage: selectedType.image, coordinate: CLLocationCoordinate2D(latitude: CLLocationDegrees(location.latitude), longitude: CLLocationDegrees(location.longitude)))
+                        .tint(selectedType.color)
+                }
+            } else {
+                ForEach(LocationType.allCases, id: \.self) { locationType in
+                    ForEach(locationType.verifiedLocations, id: \.self) { location in
+                        Marker(location.name, systemImage: locationType.image, coordinate: CLLocationCoordinate2D(latitude: CLLocationDegrees(location.latitude), longitude: CLLocationDegrees(location.longitude)))
+                            .tint(locationType.color)
+                    }
                 }
             }
         }
@@ -28,6 +36,30 @@ struct MapView: View {
             MapUserLocationButton()
             MapPitchToggle()
         }
+        .safeAreaInset(edge: .bottom) {
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    VStack {
+                        ForEach(LocationType.allCases, id: \.self) { locationType in
+                            Button(action: {
+                                self.selectedLocationType = locationType
+                            }) {
+                                Image(systemName: locationType.image)
+                                    .foregroundColor(locationType == selectedLocationType ? .white : Color.blackColor)
+                                    .font(.subheadline)
+                                    .padding()
+                                    .background(locationType == selectedLocationType ? locationType.color.opacity(0.8) : .white)
+                                    .cornerRadius(20)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
+        }
+        
         .onAppear{
             CLLocationManager().requestWhenInUseAuthorization()
         }
