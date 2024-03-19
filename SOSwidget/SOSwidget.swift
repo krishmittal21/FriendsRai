2ce123f89b7b5af49aa8772499d9c81b24b573d2
Krish Mittal <krishmittal212005@gmail.com>
2024-03-19 15:35:28 +0530

    sos button

--- SOSwidget/SOSwidget.swift
+++ SOSwidget/SOSwidget.swift
@@ -10,47 +10,42 @@ import SwiftUI
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
+        SimpleEntry(date: Date())
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        let entry = SimpleEntry(date: Date())
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let entry = SimpleEntry(date: Date())
+        let timeline = Timeline(entries: [entry], policy: .atEnd)
         completion(timeline)
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let emoji: String
 }
 
 struct SOSwidgetEntryView : View {
     var entry: Provider.Entry
+    @State private var isTapped: Bool = false
 
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
-        }
-    }
+        ZStack{
+            Circle()
+                .frame(width: 300)
+                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.orange, .red]), startPoint: .top, endPoint: .bottom))
+                .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 5)
+                .scaleEffect(isTapped ? 1.2 : 1.0)
+                .animation(.bouncy)
+            
+            Text("SOS")
+                .font(.system(size: 40, weight: .bold))
+                .foregroundStyle(.white)
+        }    }
 }
 
 struct SOSwidget: Widget {
@@ -67,14 +62,14 @@ struct SOSwidget: Widget {
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("SOS Widget")
+        .description("Tap to send SOS.")
+        .supportedFamilies([.systemSmall])
     }
 }
 
 #Preview(as: .systemSmall) {
     SOSwidget()
 } timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
+    SimpleEntry(date: .now)
 }
