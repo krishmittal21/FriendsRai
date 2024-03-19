9f23f1ee9022eb5e8bd9f4e5caa763c1cd1664b7
Krish Mittal <krishmittal212005@gmail.com>
2024-03-19 15:05:09 +0530

    widget target

---
+++ SOSwidget/SOSwidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SOSwidgetBundle.swift
+//  SOSwidget
+//
+//  Created by Krish Mittal on 19/03/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SOSwidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SOSwidget()
+    }
+}
